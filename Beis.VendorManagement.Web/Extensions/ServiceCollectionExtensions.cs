cd0201bfcd313084b7e86fae7c8283454dcad21b
emailtokvk <karthik@belaku.co.uk>
2022-06-01 17:50:58 +0100

    set min log level

--- Beis.VendorManagement.Web/Extensions/ServiceCollectionExtensions.cs
+++ Beis.VendorManagement.Web/Extensions/ServiceCollectionExtensions.cs
@@ -27,7 +27,11 @@ namespace Beis.VendorManagement.Web.Extensions
         internal static void RegisterAllServices(this IServiceCollection services, IConfiguration configuration, string nonce, bool callMockApi = false)
         {
             services.AddSession(options => options.Cookie.IsEssential = false);
-            services.AddLogging(options => options.AddConsole());
+            services.AddLogging(options =>
+            {
+                options.AddConsole();
+                options.SetMinimumLevel(LogLevel.Information);
+            });
             services.AddApplicationInsightsTelemetry(configuration["AzureMonitorInstrumentationKey"]);
             services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
                 .AddMicrosoftIdentityWebApp(options =>
